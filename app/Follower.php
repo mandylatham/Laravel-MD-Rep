84aa96ceabf6fae2194a64653d8eb3d11dad3781
mandylatham <mandylthm@gmail.com>
2021-04-20 21:50:04 +0800

    Updated follow controller

--- app/Follower.php
+++ app/Follower.php
@@ -20,10 +20,14 @@ if (! function_exists('digits_only')) {
     function digits_only($digits)
     function digits_only($digits)
     {
+    {
+    {
         return preg_replace("/[^0-9]+/", "", $digits);
     }
 }
  * @param  string $digits
+ * @return int
+ * @return int
  * @return int
  */
 if (! function_exists('digits_only')) {
